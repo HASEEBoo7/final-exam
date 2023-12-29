e7693bf8af4a09b8795bba971c8f46e1666256e0
Haseeb Raja <143373960+HASEEBoo7@users.noreply.github.com>
2023-12-28 23:00:27 -0800

    Add files via upload

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+$db_host = 'localhost';
+$db_user = 'root';
+$db_password = '';
+$db_name = 'opp';
+
+$connection = mysqli_connect("localhost", 'root', '', 'oops');
+
+// if ($connection) {
+//     echo "We are connected";
+// }
+
+
+?>
